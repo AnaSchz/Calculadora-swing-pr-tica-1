0a4f052a49aa59c9d66c75a0902c5ded8315e0b1
AnaSchz <mckagan99@gmail.com>
2017-08-16 22:01:06 -0500

    Inicio del proyecto

---
+++ src/nuevo/Nuevo.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nuevo;
+
+/**
+ *
+ * @author Marvella Ortega
+ */
+public class Nuevo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+	calculadora.setVisible(true);
+        int n=0;
+	    /**
+	    *@deprecated La variable es obsoleta
+	    */
+        System.out.println("Hola mundo");
+    }
+    
+}
